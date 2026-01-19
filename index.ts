bf5ba50b000ebef8f1066f63b56a33d54bce06e2
Yuragranovsky <yuragranovsky@gmail.com>
2026-01-19 11:49:11 +0200

    Intermediate commit

--- index.ts
+++ index.ts
@@ -7,14 +7,14 @@ import {InitiateAuthCommand, RespondToAuthChallengeCommand,
 const{region, clientId} = getRegionClientId()
 async function main() {
     const cli: readline.Interface = readline.createInterface({input: process.stdin, output: process.stdout})
-    const {username, password} = getUsernamePassword(cli)
-    const client = new CognitoIdentityProviderClient({})
-    const resp: InitiateAuthCommandOutput = initialAuthentication(client, username, password)
+    const {username, password} = await getUsernamePassword(cli)
+    const client = new CognitoIdentityProviderClient({region})
+    const resp: InitiateAuthCommandOutput = await initialAuthentication(client, username, password)
     if(resp.AuthenticationResult){
         printTokens(resp.AuthenticationResult);
     } else {
-        const newPassword = getNewPassword(cli)
-        const resp2: InitiateAuthCommandOutput = respondAuthentication(resp, client, username, newPassword)
+        const newPassword = await getNewPassword(cli)
+        const resp2: InitiateAuthCommandOutput = await respondAuthentication(resp, client, username, newPassword)
         printTokens(resp2.AuthenticationResult)
     }
 }
@@ -22,3 +22,64 @@ main().catch(err => {
     const errorMessage = err.message;
     console.error(err.$fault==="client" ? "client error: " : "server error: " + errorMessage)
 })
+
+function getRegionClientId(): { region: string; clientId: string } {
+    const region = process.env.REGION || "us-east-1"
+    const clientId = process.env.CLIENT_ID
+    if(!clientId) {
+        throw Error("missing Client Id value")
+    }
+    return {region, clientId}
+}
+async function getUsernamePassword(cli: readline.Interface): Promise<{ username: any; password: any }> {
+    const username:string = await promptLine(cli, "USERNAME: ")
+    const password:string = await promptLine(cli, "PASSWORD: ")
+    return {username, password}
+
+}
+
+async function promptLine(cli: readline.Interface, prompt: string): Promise<string> {
+    const value:string = await cli.question(prompt)
+    return (value ?? "").trim();
+}
+
+async function initialAuthentication(client: CognitoIdentityProviderClient, username: any, password: any): Promise<InitiateAuthCommandOutput> {
+    const command = new InitiateAuthCommand({
+       AuthFlow: "USER_PASSWORD_AUTH",
+       ClientId: clientId,
+       AuthParameters: {
+        USERNAME: username,
+        PASSWORD: password,
+       }
+    })
+    return client.send(command);
+}
+
+function printTokens(authenticationResult: AuthenticationResultType): void {
+    console.log("access token: ",authenticationResult.AccessToken)
+    console.log("_".repeat(40))
+    console.log("id token: ", authenticationResult.IdToken)
+    console.log("_".repeat(40))
+    console.log("refresh token: ", authenticationResult.RefreshToken)
+}
+async function getNewPassword(cli: readline.Interface): Promise<string>{
+    return promptLine(cli, "NEW PASSWORD: ")
+}
+
+async function respondAuthentication(resp: InitiateAuthCommandOutput, client: CognitoIdentityProviderClient, username: any, newPassword: string): Promise<InitiateAuthCommandOutput> {
+    if (resp.ChallengeName != "NEW_PASSWORD_REQUIRED"){
+        throw Error(`Unknow Challenge Name ${resp.ChallengeName}`)
+    }
+
+    const command = new RespondToAuthChallengeCommand({
+       ChallengeName:"NEW_PASSWORD_REQUIRED",
+       ClientId: clientId,
+       Session: resp.Session,
+       ChallengeResponses:{
+        USERNAME: username,
+        NEW_PASSWORD: newPassword
+       }
+    });
+    return client.send(command)
+}
+
